6992e29ca67111ccef4a1bdcc866cdf11a1d3d17
Diego Castro Satt <diegocastrosatt@gmail.com>
2026-06-28 19:47:17 -0400

    agrego funcion para calculo de moneda respecto al dolar

--- Src/conversiones.js
+++ Src/conversiones.js
@@ -1,4 +1,9 @@
-const { unidades, categoriaDe } = require("./unidades");
+const dependencias = typeof require === "function"
+    ? require("./unidades")
+    : { unidades, categoriaDe };
+
+const unidadesDisponibles = dependencias.unidades;
+const buscarCategoria = dependencias.categoriaDe;
 
 function convertirTemperatura(valor, origen, destino) {
     if (origen === destino) return valor;
@@ -20,37 +25,18 @@ function convertirTemperatura(valor, origen, destino) {
     throw new Error("Conversion de temperatura no disponible.");
 }
 
-function convertirConsumo(valor, origen, destino) {
-    if (origen === destino) return valor;
-
-    let kmPorLitro;
-
-    if (origen === "km_l") kmPorLitro = valor;
-    else if (origen === "litros_100km") kmPorLitro = 100 / valor;
-    else if (origen === "mpg_us") kmPorLitro = valor * 0.425144;
-    else if (origen === "mpg_uk") kmPorLitro = valor * 0.354006;
-    else throw new Error("Unidad de consumo no disponible.");
-
-    if (destino === "km_l") return kmPorLitro;
-    if (destino === "litros_100km") return 100 / kmPorLitro;
-    if (destino === "mpg_us") return kmPorLitro / 0.425144;
-    if (destino === "mpg_uk") return kmPorLitro / 0.354006;
-
-    throw new Error("Conversion de consumo no disponible.");
-}
-
 function convertirUnidad(valor, origen, destino) {
     valor = Number(valor);
     origen = origen.toLowerCase();
     destino = destino.toLowerCase();
 
-    const categoria = categoriaDe(origen);
+    const categoria = buscarCategoria(origen);
 
     if (Number.isNaN(valor)) {
         throw new Error("Ingrese un numero valido.");
     }
 
-    if (!categoria || categoria !== categoriaDe(destino)) {
+    if (!categoria || categoria !== buscarCategoria(destino)) {
         throw new Error("Conversion no disponible.");
     }
 
@@ -62,15 +48,13 @@ function convertirUnidad(valor, origen, destino) {
         return convertirTemperatura(valor, origen, destino);
     }
 
-    if (categoria === "consumo_combustible") {
-        if (valor === 0) {
-            throw new Error("El valor no puede ser cero en consumo de combustible.");
-        }
-
-        return convertirConsumo(valor, origen, destino);
+    if (categoria === "moneda") {
+        return valor * unidadesDisponibles.moneda[destino] / unidadesDisponibles.moneda[origen];
     }
 
-    return valor * unidades[categoria][origen] / unidades[categoria][destino];
+    return valor * unidadesDisponibles[categoria][origen] / unidadesDisponibles[categoria][destino];
 }
 
-module.exports = { convertirUnidad };
+if (typeof module !== "undefined") {
+    module.exports = { convertirUnidad };
+}
